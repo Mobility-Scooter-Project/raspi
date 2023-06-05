12c1d2c0274876e220994ffe2eb8a71a2b291917
huangruoqi <44049919+huangruoqi@users.noreply.github.com>
2023-06-05 14:58:31 -0700

    Update setup.sh

--- video_analysis/setup.sh
+++ video_analysis/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 source /home/pi/.bashrc
