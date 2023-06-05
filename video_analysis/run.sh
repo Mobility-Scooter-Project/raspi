9e58cd6aa437c99bec68a0ed4155d25b8df43df0
Ruoqi Huang <huangruoqi374@gmail.com>
2023-06-05 16:51:56 -0700

    change script

---
+++ video_analysis/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/pi/Desktop/raspi/video_analysis/
+source /home/pi/miniforge3/etc/profile.d/conda.sh
+conda activate video2
+python main.py
--- video_analysis/setup.sh
+++ video_analysis/setup.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 sudo apt update
 sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
-conda create -n video python=3.7 -y
-conda activate video
 pip install -r requirements.txt
 pip install ./assets/mediapipe-0.8-cp37-cp37m-linux_aarch64.whl
-pip install protobuf==3.20.*
+pip install protobuf==3.19.*
 
 
 
