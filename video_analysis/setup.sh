0a8b43b35fee030c3426dcb0add207dca226f51a
huangruoqi <44049919+huangruoqi@users.noreply.github.com>
2023-06-05 14:18:20 -0700

    Update setup.sh

--- video_analysis/setup.sh
+++ video_analysis/setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install python3-opencv -y  
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 curl -L https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh > install.sh
 chmod +x install.sh
 ./install.sh
@@ -8,5 +8,11 @@ conda config --set auto_activate_base false
 source ~/.bashrc
 conda create -n video python=3.7 -y
 conda activate video
+pip install -r requirements.txt
+pip install ./assets/mediapipe-0.8-cp37-cp37m-linux_aarch64.whl
+pip install protobuf==3.20.*
+
+
+
 
 
